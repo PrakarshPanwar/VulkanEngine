e8883932cdc7f897a7d222898d66e722fe68f2bb
PrakarshPanwar <prakarshpanwar@gmail.com>
2022-08-06 08:26:24 +0530

    Premake Project Structure Modified

---
+++ Dependencies.lua
@@ -0,0 +1,26 @@
+
+-- VulkanCore Dependencies
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/VulkanCore/vendor/glfw/include"
+IncludeDir["stb_image"] = "%{wks.location}/VulkanCore/vendor/stb_image"
+IncludeDir["entt"] = "%{wks.location}/VulkanCore/vendor/entt"
+IncludeDir["glm"] = "%{wks.location}/VulkanCore/vendor/glm"
+IncludeDir["ImGui"] = "%{wks.location}/VulkanCore/vendor/imgui"
+IncludeDir["TinyObjLoader"] = "%{wks.location}/VulkanCore/vendor/tinyobjloader"
+IncludeDir["VulkanSDK"] = "%{wks.location}/VulkanCore/vendor/VulkanSDK/Include"
+
+LibraryDir = {}
+LibraryDir["VulkanSDK"] = "%{wks.location}/VulkanCore/vendor/VulkanSDK/Lib"
+
+Library = {}
+Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
+
+Library["ShaderC_Debug"] = "%{LibraryDir.VulkanSDK}/shaderc_sharedd.lib"
+Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK}/spirv-cross-cored.lib"
+Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsld.lib"
+Library["SPIRV_Tools_Debug"] = "%{LibraryDir.VulkanSDK}/SPIRV-Toolsd.lib"
+
+Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
+Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-core.lib"
+Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsl.lib"
