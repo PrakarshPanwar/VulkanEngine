24d6079ff7f06400e56703884f6883a8b6ea270a
PrakarshPanwar <prakarshpanwar@gmail.com>
2022-11-21 19:55:15 +0530

    Implemented Mesh addition in SceneHierarchyPanel
    - Renamed `VulkanMesh` to `Mesh`
    
    # Conflicts:
    #	VulkanCore/src/Platform/Vulkan/Mesh.cpp
    
    Fixed few errors caused by Cherry-Pick commit
    
    Moved Mesh files
    
    - Also corrected its include in other files

--- VulkanCore/src/VulkanCore/Renderer/Renderer.cpp
+++ VulkanCore/src/VulkanCore/Renderer/Renderer.cpp
@@ -78,7 +78,7 @@ namespace VulkanCore {
 		vkCmdDraw(drawCmd, 3, 1, 0, 0);
 	}
 
-	void Renderer::RenderMesh(std::shared_ptr<VulkanMesh> mesh)
+	void Renderer::RenderMesh(std::shared_ptr<Mesh> mesh)
 	{
 		mesh->Bind(m_CommandBuffers[GetCurrentFrameIndex()]);
 		mesh->Draw(m_CommandBuffers[GetCurrentFrameIndex()]);
