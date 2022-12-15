9e6d223942143fa97597b1c1bdda733b428b2419
PrakarshPanwar <prakarshpanwar@gmail.com>
2022-12-15 13:45:31 +0530

    Implemented Render Thread in Vulkan Vertex and Index Buffer
    - This is not working right now will be seen in future

--- VulkanCore/src/Platform/Vulkan/VulkanVertexBuffer.cpp
+++ VulkanCore/src/Platform/Vulkan/VulkanVertexBuffer.cpp
@@ -2,6 +2,7 @@
 #include "VulkanVertexBuffer.h"
 
 #include "VulkanCore/Core/Core.h"
+#include "VulkanCore/Renderer/Renderer.h"
 #include "VulkanAllocator.h"
 
 namespace VulkanCore {
@@ -9,45 +10,48 @@ namespace VulkanCore {
 	VulkanVertexBuffer::VulkanVertexBuffer(void* data, uint32_t size)
 		: m_Size(size)
 	{
-		auto device = VulkanContext::GetCurrentDevice();
-		VulkanAllocator allocator("VertexBuffer");
+		Renderer::Submit([this, data, size]
+		{
+			auto device = VulkanContext::GetCurrentDevice();
+			VulkanAllocator allocator("VertexBuffer");
 
-		// Staging Buffer
-		VkBufferCreateInfo bufferCreateInfo{};
-		bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-		bufferCreateInfo.size = m_Size;
-		bufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-		bufferCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+			// Staging Buffer
+			VkBufferCreateInfo bufferCreateInfo{};
+			bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+			bufferCreateInfo.size = m_Size;
+			bufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+			bufferCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
-		VkBuffer stagingBuffer;
-		VmaAllocation stagingBufferAlloc = allocator.AllocateBuffer(bufferCreateInfo, VMA_MEMORY_USAGE_AUTO_PREFER_HOST, stagingBuffer);
+			VkBuffer stagingBuffer;
+			VmaAllocation stagingBufferAlloc = allocator.AllocateBuffer(bufferCreateInfo, VMA_MEMORY_USAGE_AUTO_PREFER_HOST, stagingBuffer);
 
-		// Copy/Map Data to Staging Buffer
-		uint8_t* dstData = allocator.MapMemory<uint8_t>(stagingBufferAlloc);
-		memcpy(dstData, data, size);
-		allocator.UnmapMemory(stagingBufferAlloc);
+			// Copy/Map Data to Staging Buffer
+			uint8_t* dstData = allocator.MapMemory<uint8_t>(stagingBufferAlloc);
+			memcpy(dstData, data, size);
+			allocator.UnmapMemory(stagingBufferAlloc);
 
-		VkBufferCreateInfo vertexBufferCreateInfo{};
-		vertexBufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-		vertexBufferCreateInfo.size = m_Size;
-		vertexBufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-		m_MemoryAllocation = allocator.AllocateBuffer(vertexBufferCreateInfo, VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE, m_VulkanBuffer);
+			VkBufferCreateInfo vertexBufferCreateInfo{};
+			vertexBufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+			vertexBufferCreateInfo.size = m_Size;
+			vertexBufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+			m_MemoryAllocation = allocator.AllocateBuffer(vertexBufferCreateInfo, VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE, m_VulkanBuffer);
 
-		VkCommandBuffer copyCmd = device->GetCommandBuffer();
+			VkCommandBuffer copyCmd = device->GetCommandBuffer();
 
-		VkBufferCopy copyRegion{};
-		copyRegion.size = m_Size;
+			VkBufferCopy copyRegion{};
+			copyRegion.size = m_Size;
 
-		vkCmdCopyBuffer(copyCmd,
-			stagingBuffer,
-			m_VulkanBuffer,
-			1,
-			&copyRegion
-		);
+			vkCmdCopyBuffer(copyCmd,
+				stagingBuffer,
+				m_VulkanBuffer,
+				1,
+				&copyRegion
+			);
 
-		device->FlushCommandBuffer(copyCmd);
+			device->FlushCommandBuffer(copyCmd);
 
-		allocator.DestroyBuffer(stagingBuffer, stagingBufferAlloc);
+			allocator.DestroyBuffer(stagingBuffer, stagingBufferAlloc);
+		});
 	}
 
 	VulkanVertexBuffer::~VulkanVertexBuffer()
