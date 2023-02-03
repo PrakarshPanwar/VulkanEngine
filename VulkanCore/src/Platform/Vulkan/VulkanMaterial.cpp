67d43b5270f2558fdeb8bc726d3555ee4cd997b5
PrakarshPanwar <prakarshpanwar@gmail.com>
2023-02-03 17:43:48 +0530

    Added new members in VulkanMaterial and Shader

--- VulkanCore/src/Platform/Vulkan/VulkanMaterial.cpp
+++ VulkanCore/src/Platform/Vulkan/VulkanMaterial.cpp
@@ -1,10 +1,18 @@
 #include "vulkanpch.h"
 #include "VulkanMaterial.h"
+#include "VulkanSwapChain.h"
+#include "VulkanCore/Core/Application.h"
 
 namespace VulkanCore {
 
 	VulkanMaterial::VulkanMaterial(std::shared_ptr<Shader> shader, const std::string& debugName)
 	{
+		auto vulkanDstPool = Application::Get()->GetDescriptorPool();
+
+		VkDescriptorSetLayout materialSetLayout = shader->CreateDescriptorSetLayout(0)->GetDescriptorSetLayout();
+		for (uint32_t i = 0; i < VulkanSwapChain::MaxFramesInFlight; ++i)
+			vulkanDstPool->AllocateDescriptorSet(materialSetLayout, m_MaterialDescriptorSets[i]);
+
 
 	}
 
--- VulkanCore/src/Platform/Vulkan/VulkanMaterial.h
+++ VulkanCore/src/Platform/Vulkan/VulkanMaterial.h
@@ -15,8 +15,11 @@ namespace VulkanCore {
 		std::shared_ptr<Shader> m_Shader;
 		std::string m_DebugName;
 
+		// NOTE: Material Descriptor Set is Set 0, rest are Pending Descriptor Sets
+		std::vector<VkDescriptorSet> m_MaterialDescriptorSets;
+		std::unordered_map<uint32_t, std::vector<VkWriteDescriptorSet>> m_MaterialWriteDescriptors;
 		// Key: Binding ID, Value: 3-sized Descriptor Sets vector
-		std::unordered_map<uint32_t, std::vector<VkDescriptorSet>> m_MaterialDescriptorSets;
+		std::unordered_map<uint32_t, std::vector<VkDescriptorSet>> m_PendingDescriptorSets;
 	};
 
 }
