e3976370345409bdc8eb6cea086d67145394e529
PrakarshPanwar <prakarshpanwar@gmail.com>
2023-03-02 13:03:43 +0530

    Added CursorMode and modified Timestamp methods

--- VulkanCore/src/Platform/Windows/WindowsInput.cpp
+++ VulkanCore/src/Platform/Windows/WindowsInput.cpp
@@ -6,11 +6,35 @@
 
 namespace VulkanCore {
 
+	namespace Utils {
+
+		static int GetGLFWCursorMode(CursorMode cursorMode)
+		{
+			switch (cursorMode)
+			{
+			case CursorMode::Normal: return GLFW_CURSOR_NORMAL;
+			case CursorMode::Hidden: return GLFW_CURSOR_HIDDEN;
+			case CursorMode::Locked: return GLFW_CURSOR_DISABLED;
+			default:
+				VK_CORE_ASSERT(false, "Cursor Mode not found");
+				return GLFW_CURSOR_NORMAL;
+			}
+		}
+
+	}
+
 	bool Input::IsKeyPressed(KeyCode key)
 	{
 		GLFWwindow* window = (GLFWwindow*)Application::Get()->GetWindow()->GetNativeWindow();
 		auto state = glfwGetKey(window, key);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	void Input::SetCursorMode(CursorMode cursorMode)
+	{
+		GLFWwindow* window = (GLFWwindow*)Application::Get()->GetWindow()->GetNativeWindow();
+		glfwSetInputMode(window, GLFW_CURSOR, Utils::GetGLFWCursorMode(cursorMode));
+		glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 	}
 
 	bool Input::IsMouseButtonPressed(MouseCode button)
