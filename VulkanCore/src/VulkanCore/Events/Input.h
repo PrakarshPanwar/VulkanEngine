ca9e5776e388185941556b618578ef5ae6c6c2d6
PrakarshPanwar <prakarshpanwar@gmail.com>
2023-03-02 13:03:43 +0530

    Added CursorMode and modified Timestamp methods

--- VulkanCore/src/VulkanCore/Events/Input.h
+++ VulkanCore/src/VulkanCore/Events/Input.h
@@ -4,10 +4,18 @@
 
 namespace VulkanCore {
 
+	enum class CursorMode
+	{
+		Normal,
+		Hidden,
+		Locked
+	};
+
 	class Input
 	{
 	public:
 		static bool IsKeyPressed(KeyCode key);
+		static void SetCursorMode(CursorMode cursorMode);
 		static bool IsMouseButtonPressed(MouseCode button);
 		static std::pair<float, float> GetMousePosition();
 	};
