8fe2e0c5e2e6821f5d3bbb2c3bc20abf7b583033
prakarshpanwar <prakarshpanwar@gmail.com>
2024-09-21 13:30:48 +0530

    Updated Assimp Submodule and Project Build Batch File
    - Updated README.md

--- VkGenProjects.bat
+++ VkGenProjects.bat
@@ -1,2 +1,15 @@
+@echo off
 call VulkanCore\vendor\premake\premake5.exe vs2022
+set /p input=BUILD ASSIMP SOLUTION(Y/N)=
+
+IF %input%==Y (
+cd VulkanCore\vendor\assimp
+cmake -DASSIMP_BUILD_ZLIB=ON CMakeLists.txt
+)
+
+IF %input%==y (
+cd VulkanCore\vendor\assimp
+cmake -DASSIMP_BUILD_ZLIB=ON CMakeLists.txt
+)
+
 pause
