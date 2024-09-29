8422fc65de23bce00b0ca1834d0c373201dd5450
PrakarshPanwar <prakarshpanwar@gmail.com>
2024-09-29 11:58:34 +0530

    Modified Batch Script
    - Will check if Assimp solution exists

--- VkGenProjects.bat
+++ VkGenProjects.bat
@@ -1,2 +1,24 @@
+@echo off
 call VulkanCore\vendor\premake\premake5.exe vs2022
+
+IF NOT EXIST VulkanCore\vendor\assimp\Assimp.sln (
+    goto buildAssimp
+) ELSE (
+    pause
+    EXIT
+)
+
+:buildAssimp
+set /p input=BUILD ASSIMP SOLUTION(Y/N)=
+
+IF %input%==Y (
+cd VulkanCore\vendor\assimp
+cmake -DASSIMP_BUILD_ZLIB=ON CMakeLists.txt
+)
+
+IF %input%==y (
+cd VulkanCore\vendor\assimp
+cmake -DASSIMP_BUILD_ZLIB=ON CMakeLists.txt
+)
+
 pause
