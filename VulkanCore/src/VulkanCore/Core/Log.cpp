a93105d5ffdf4dce12c88093e9aeae1cda1e4da2
PrakarshPanwar <prakarshpanwar@gmail.com>
2025-05-02 20:02:32 +0530

    Added Log Debug with Custom Colors

--- VulkanCore/src/VulkanCore/Core/Log.cpp
+++ VulkanCore/src/VulkanCore/Core/Log.cpp
@@ -12,7 +12,13 @@ namespace VulkanCore {
 	void Log::Init()
 	{
 		std::vector<spdlog::sink_ptr> logSinks;
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+
+		auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		consoleSink->set_color(spdlog::level::trace, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+		consoleSink->set_color(spdlog::level::info, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		consoleSink->set_color(spdlog::level::debug, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+
+		logSinks.emplace_back(consoleSink);
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("VulkanEngine.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
